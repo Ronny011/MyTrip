6b92b9799a1315870164337fe3d8b44babbb845d
Ronnyn011 <48674898+Ronny011@users.noreply.github.com>
2020-06-11 04:41:08 +0300

    cosmetics
    changed splash screen delay time
    changed splash activity to full screen
    changed theme colors
    changed app icon
    changed splash screen style

--- app/src/main/java/com/example/mytrip/SplashScreen.java
+++ app/src/main/java/com/example/mytrip/SplashScreen.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends Activity
 {
@@ -12,6 +14,8 @@ public class SplashScreen extends Activity
         protected void onCreate(Bundle savedInstanceState)
         {
             super.onCreate(savedInstanceState);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
             setContentView(R.layout.splash_screen);
 
             handler = new Handler();
@@ -25,6 +29,6 @@ public class SplashScreen extends Activity
                         startActivity(intent);
                         finish();
                     }
-                },650);
+                },750);
         }
 }
